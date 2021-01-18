c4670fceeee0c9252a32d50912a00579b4f07a22
Muni <kangmoonee@gmail.com>
2021-01-18 22:29:53 +0900

    JavaScript:moreAndLess Btn

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+const moreBtn = document.querySelector(".info .secondBox .contents .moreBtn");
+const contents = document.querySelector(".info .secondBox .contents .title");
+moreBtn.addEventListener("click", () => {
+    contents.classList.toggle("clamp");
+    if (moreBtn.innerText === "더보기") {
+        moreBtn.innerText = "간략히"
+    }
+    else {
+        moreBtn.innerText = "더보기"
+    }
+  });
