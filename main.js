295e3130b2fd9c99076f49922db82d7ef2d1bbd3
kangmuni <kangmoonee@gmail.com>
2022-05-26 15:21:38 +0900

    Last Touch

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
-const moreBtn1 = document.querySelector('.info .secondBox .contents .moreBtn');
-const contents = document.querySelector('.info .secondBox .contents .title');
-const items = document.querySelectorAll('.upNext .item');
-console.log(items);
-const moreBtn2 = document.querySelector('.upNext .item .moreBtn');
+const contentsBtn = document.querySelector('.contents__moreBtn');
+const contents = document.querySelector('.contents__title');
 
-moreBtn1.addEventListener('click', () => {
+contentsBtn.addEventListener('click', () => {
   contents.classList.toggle('clamp');
-  if (moreBtn1.innerText === '더보기') {
-    moreBtn1.innerText = '간략히';
+  if (contentsBtn.innerText === '더보기') {
+    contentsBtn.innerText = '간략히';
   } else {
-    moreBtn1.innerText = '더보기';
+    contentsBtn.innerText = '더보기';
   }
-});
-
-items.forEach((item) => {
-  console.log(item);
-  item.addEventListener('mouseover', (event) => {
-    console.log(event.target);
-    if (moreBtn2.style.display === 'none') {
-      moreBtn2.style.display = 'block';
-    }
-  });
 });
